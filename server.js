dab779c98f0e9d05cc6212f85fa477239abfc08c
masoudvrz <masoud.varzi@yahoo.com>
2023-11-20 12:43:50 +0330

    refactor: moved books api to controller

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("node:url");
 const db = require("./db.json");
+const {getAllBook , removeBookById} = require("./controllers/bookController");
 
 const server = http.createServer((req, res) => {
   // get list of users
@@ -16,35 +17,10 @@ const server = http.createServer((req, res) => {
     });
   } // get list of books
   else if (req.method === "GET" && req.url === "/api/books") {
-    fs.readFile("./db.json", (err, db) => {
-      if (err) throw err;
-
-      const books = JSON.stringify(JSON.parse(db).books);
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(books);
-      res.end();
-    });
+    getAllBook(req , res);
   } // delete specific book
   else if (req.method === "DELETE" && req.url.startsWith("/api/books")) {
-    console.log(db, "db");
-    const parsedUrl = url.parse(req.url, true);
-    const bookId = parsedUrl.query.id;
-    const updatedBookList = db.books.filter((book) => book.id !== +bookId);
-    if (updatedBookList.length !== db.books.length) {
-      fs.writeFile(
-        "./db.json",
-        JSON.stringify({ ...db, books: updatedBookList }),
-        (err) => {
-          if (err) throw err;
-        }
-      );
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(JSON.stringify({ massage: "Book Is Removed Successfully" }));
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.write(JSON.stringify({ massage: "Can not Found This Book" }));
-    }
-    res.end();
+    removeBookById(req, res)
   } // add new book
   else if (req.method === "POST" && req.url === "/api/books") {
     let body = "";
