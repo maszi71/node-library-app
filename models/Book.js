211d8d2a3c8512f28227bcbd091165cd6d649090
masoudvrz <masoud.varzi@yahoo.com>
2023-12-02 18:11:30 +0330

    refactor: updated findAll andBook and removeBook

--- models/Book.js
+++ models/Book.js
@@ -1,38 +1,22 @@
-const fs = require("fs");
-const path = require("node:path");
+const { dbConnection } = require("../config/db");
+const { ObjectId } = require("mongodb");
 
-const findAllBook = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile("./db.json", (err, db) => {
-      if (err) reject(err);
-      resolve(JSON.parse(db).books);
-    });
-  });
+const findAllBook = async () => {
+  const db = await dbConnection();
+  const bookCollection = db.collection("books");
+  const allBook = bookCollection.find({}).toArray();
+  return allBook;
 };
 
-const removeBook = (bookId) => {
-  return new Promise((resolve, reject) => {
-    console.log(path.join(__dirname, "db.json"))
-    fs.readFile( "db.json", (err, db) => {
-      if (err) reject(err);
-      const parsedDb = JSON.parse(db);
-      console.log(parsedDb)
-      const updatedBookList = parsedDb.books.filter((book) => book.id !== +bookId);
-     console.log(updatedBookList)
-      if (updatedBookList.length !== parsedDb.books.length) {
-        fs.writeFile(
-          path.join(__dirname, "db.json"),
-          JSON.stringify({ ...parsedDb, books: updatedBookList }),
-          (err) => {
-            if (err) reject(err);
-            resolve({ massage: "Book Is Removed Successfully" });
-          }
-        );
-      } else {
-        reject({ massage: "Can not Found This Book" });
-      }
-    });
-  });
+const removeBook = async (bookId) => {
+  const db = await dbConnection();
+  const bookCollection = db.collection("books");
+  const result = await bookCollection.deleteOne({ _id: new ObjectId(bookId) });
+  if (result.deletedCount) {
+    return { massage: "Book Is Removed Successfully" };
+  } else {
+    return { massage: "Can not Found This Book" };
+  }
 };
 
 module.exports = {
